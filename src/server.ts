0556c2ccfeefd5e4cd43ee222aefd5df3ab2d5cd
datata <dani@danitarazona.dev>
2024-06-28 11:08:15 +0200

    feat: port to const

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import express from 'express';
 
 const app = express();
 
+const PORT = process.env.PORT || 4000
+
 app.get('/healthy', (req, res) => {
   res.send('Server is healthy');
 })
 
-app.listen(4000, () => {
-  console.log(`Server is running `)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
 
 
