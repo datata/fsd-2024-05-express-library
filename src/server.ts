618b93b156ca12db0bc90cf1c2303800d641e65e
datata <dani@danitarazona.dev>
2024-06-28 11:57:10 +0200

    feat: books crud

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,23 @@ app.get('/healthy', (req, res) => {
   res.send('Server is healthy');
 })
 
+// BOOKS
+app.get('/books', (req, res) => {
+  res.send('GET ALL BOOKS')
+})
+
+app.post('/books', (req, res) => {
+  res.send('BOOK created')
+})
+
+app.put('/books', (req, res) => {
+  res.send('BOOK UPDATED')
+})
+
+app.delete('/books', (req, res) => {
+  res.send('book deleted')
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
