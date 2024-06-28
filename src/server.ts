436cad69fb97a3a9faf0c7b3273c10a6917e9b51
datata <dani@danitarazona.dev>
2024-06-28 12:41:41 +0200

    feat: authors crud

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,34 @@ import express from 'express';
 
 const app = express();
 
+// middleware
+app.use(express.json())
+
 const PORT = process.env.PORT || 4000
 
 app.get('/healthy', (req, res) => {
   res.send('Server is healthy');
+})
+
+//  AUTHORS
+app.post('/authors', (req, res) => {
+  // recuperar la informacion de la req
+  console.log(req.body);
+  console.log(req.body.name);
+  console.log(req.body.nationality);
+
+  res.send('CREATE AUTHOR')
+})
+
+// rutas dinamincas usamos req params
+app.put('/authors/:id', (req, res) => {
+  console.log(req.params.id);
+  
+  res.send(`AUTHOR UPDATED with id: ${req.params.id}`)
+})
+
+app.delete('/authors/:id', (req, res) => {
+  res.send(`AUTHOR DELETED BY ID ${req.params.id}`)
 })
 
 // BOOKS
