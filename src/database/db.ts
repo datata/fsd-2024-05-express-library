a288cba095649eea2ca737da426cc8e418bf9c7d
datata <dani@danitarazona.dev>
2024-07-01 13:13:12 +0200

    feat: add user migration

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import 'dotenv/config';
 import { DataSource } from "typeorm";
 import { Author1719825025988 } from "./migrations/1719825025988-author";
+import { User1719830260277 } from "./migrations/1719830260277-user";
 
 export const AppDataSource = new DataSource({
 	type: "mysql",
@@ -11,7 +12,7 @@ export const AppDataSource = new DataSource({
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE,
 	entities: [],
-	migrations: [Author1719825025988],
+	migrations: [Author1719825025988,User1719830260277],
 	synchronize: false,
 	logging: false,
 });
