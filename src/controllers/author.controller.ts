c3dd185b4ef8fe23634bbbe74fb0459b353b8baf
datata <dani@danitarazona.dev>
2024-07-02 12:11:30 +0200

    feat: create author

--- src/controllers/author.controller.ts
+++ src/controllers/author.controller.ts
@@ -1,22 +1,62 @@
 import { Request, Response } from "express";
+import { Author } from "../database/models/Author";
 
-export const createAuthor = (req: Request, res: Response) => {
-  // recuperar la informacion de la req
-  console.log(req.body);
-  console.log(req.body.name);
-  console.log(req.body.nationality);
+export const createAuthor = async (req: Request, res: Response) => {
+  try {
+    // 1. recuperar la informacion de la req
+    const name = req.body.name;
+    const nationality = req.body.nationality;
 
-  res.json(
-    {
-      success: true,
-      message: 'CREATE AUTHOR'
+    // 2. Validar informacion
+    if (!name) {
+      return res.status(400).json(
+        {
+          success: false,
+          message: "name is required"
+        }
+      )
     }
-  )
+
+    if (!nationality) {
+      return res.status(400).json(
+        {
+          success: false,
+          message: "nationality is required"
+        }
+      )
+    }
+
+    // 3. Tratar informacion
+
+    // 4. Atacar a la bd
+    const newAuthor = await Author.create(
+      {
+        name: name,
+        nationality: nationality
+      }
+    ).save();
+
+    // 5. Responder
+    res.json(
+      {
+        success: true,
+        message: 'Author created successfully',
+        data: newAuthor
+      }
+    )
+  } catch (error) {
+    res.status(500).json(
+      {
+        success: false,
+        message: "Error creating author"
+      }
+    )
+  }
 }
 
 export const updateAuthorById = (req: Request, res: Response) => {
   console.log(req.params.id);
-  
+
   res.send(`AUTHOR UPDATED with id: ${req.params.id}`)
 }
 
