7cdcce76b36c5dfc641b3b2827a87bbd3dc84e62
datata <dani@danitarazona.dev>
2024-07-04 12:13:50 +0200

    update: add TokenData to req types

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from 'jsonwebtoken'
+import { TokenDecoded } from "../types";
 
 export const auth = (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -13,8 +14,14 @@ export const auth = (req: Request, res: Response, next: NextFunction) => {
     }
 
     const token = req.headers.authorization.split(' ')[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);    
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as TokenDecoded;    
     
+    req.tokenData = {
+      id: decoded.id,
+      role: decoded.role,
+      email: decoded.email
+    }
+
     next();
   } catch (error) {
     res.status(500).json(
